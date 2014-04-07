d0dd2c1f208db30d9b33ab81f11725bfd845e672
BrianTSao <caoqb@hotmail.com>
2014-04-07 12:57:27 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Admalls::Application.config.secret_key_base = 'ff74a69ae9051561eaac332ae4b9d3ea177a2500abae3e1f99dfa85a76d742db07d4565d0129df404dd99a22b13e0909fa7eaf8f161fe33b3beb7995bedc24d1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Admalls::Application.config.session_store :cookie_store, key: '_admalls_session'
